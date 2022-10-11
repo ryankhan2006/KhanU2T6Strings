6d1dc9aa4e450b36c224c9491a03472641206d78
unknown <BT_3W2_07@BKTECH.LOCAL>
2022-10-11 12:28:28 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        String one= "Ryan";
+        System.out.println("Hello, this is " + one);
+        //method one
+
+        String two= new String("Khan");
+        System.out.println("Hello, this is my last name: " + two);
+        //method two
+
+        String random= "I play basketball";
+        String three= new String(random);
+        System.out.println(three);
+        //method three
+
+        
+
+
+
+
+
+
+    }
+}
